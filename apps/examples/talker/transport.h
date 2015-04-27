d803b19ca5a14cd1a245c587963fe4ada97e7a07
yigitgunay <yigit.guenay@rwth-aachen.de>
2015-04-27 13:09:52 +0200

    Revert "Added talker & listener apps as a ROS prototypes. "

--- apps/examples/talker/transport.h
+++
@@ -1,7 +0,0 @@
-void transport_init();
-
-void transport_create_node(const char* name);
-
-void transport_create_publisher(const char* topic_name);
-
-void transport_publish_string(const char* topic_name, const char* msg_string);
